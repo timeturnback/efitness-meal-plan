89c130c17c7e6418cf425a318eb9d685d6144e89
timeturnback <time.turnback@gmail.com>
2022-10-02 17:02:07 +0700

    fix issue of simple select input

--- src/components/input/simple-select-input.tsx
+++ src/components/input/simple-select-input.tsx
@@ -1,65 +1,83 @@
 import classNames from 'classnames';
-import { useState } from 'react';
 
-function GenderSelect() {
-  const [clickmale, setClickMale] = useState(false);
-  const [clickfemale, setClickFemale] = useState(false);
-  const [click, setClick] = useState(true);
+import type { SelectOptionObject } from '../constants/select-options';
+import { GENDER_OPTIONS } from '../constants/select-options';
 
-  const _onHandleGenderFemale = () => {
-    if (click) {
-      setClick(false);
-      setClickFemale(true);
-    } else {
-      setClickMale(false);
-      setClickFemale(true);
-    }
-  };
-  const _onHandleGenderMale = () => {
-    if (click) {
-      setClick(false);
-      setClickMale(true);
-    } else {
-      setClickFemale(false);
-      setClickMale(true);
-    }
-  };
-
+export function SimpleSelectInput({
+  label,
+  currentValue,
+  setCurrentValue,
+  options,
+}: {
+  label: string;
+  currentValue: string;
+  setCurrentValue: (value: string) => void;
+  options: SelectOptionObject[];
+}) {
   return (
-    <div>
-      <span className="mb-3 block">Gender</span>
-      <div className="flex items-center">
-        <div
-          className="mr-5 cursor-pointer select-none rounded-md border border-black p-3"
-          onClick={_onHandleGenderMale}
-        >
-          <span className="relative flex items-center border-black pl-7 leading-4 before:absolute before:left-0 before:h-5 before:w-5 before:rounded-3xl before:border before:border-bl-ccc before:content-['']">
-            <div
-              className={classNames(
-                'absolute w-3 h-3 transition-all scale-0 opacity-0 bg-bl-222 rounded-3xl left-1',
-                clickmale && 'scale-100 opacity-100'
-              )}
-            />
-            Male
-          </span>
-        </div>
-        <div
-          className="cursor-pointer select-none rounded-md border border-black p-3"
-          onClick={_onHandleGenderFemale}
-        >
-          <span className="relative flex items-center border-black pl-7 leading-4 before:absolute before:left-0 before:h-5 before:w-5 before:rounded-3xl before:border before:border-bl-ccc before:content-['']">
-            <div
-              className={classNames(
-                'absolute w-3 h-3 transition-all scale-0 opacity-0 bg-bl-222 rounded-3xl left-1',
-                clickfemale && 'scale-100 opacity-100'
-              )}
-            />
-            Female
-          </span>
-        </div>
+    <div className="p-2">
+      <span className="mb-2 font-medium text-gray-800">{label}</span>
+      <div className="flex items-center gap-3">
+        {options.map((item) => (
+          <SelectOption
+            key={item.value}
+            data={item}
+            currentValue={currentValue}
+            onClick={setCurrentValue}
+          />
+        ))}
       </div>
     </div>
   );
 }
 
-export default GenderSelect;
+const SelectOption = ({
+  onClick,
+  data,
+  currentValue,
+}: {
+  onClick: (value: string) => void;
+  data: SelectOptionObject;
+  currentValue: string;
+}) => {
+  const { value, label } = data || {};
+  const isSelected = value === currentValue;
+
+  const _onClick = () => {
+    onClick(value);
+  };
+
+  return (
+    <button
+      className="flex items-center rounded-md bg-white p-3 drop-shadow-md"
+      onClick={_onClick}
+    >
+      <div className="relative flex items-center border-black pl-7 leading-4 before:absolute before:left-0 before:h-5 before:w-5 before:rounded-3xl before:border before:border-bl-ccc before:content-['']">
+        <div
+          className={classNames(
+            'absolute w-3 h-3 transition-all scale-0 opacity-0 bg-gray-800 rounded-3xl left-1',
+            isSelected && 'scale-100 opacity-100'
+          )}
+        />
+      </div>
+      {label}
+    </button>
+  );
+};
+
+export const GenderSelect = ({
+  currentValue,
+  setCurrentValue,
+}: {
+  currentValue: string;
+  setCurrentValue: (value: string) => void;
+}) => {
+  return (
+    <SimpleSelectInput
+      options={GENDER_OPTIONS}
+      currentValue={currentValue}
+      setCurrentValue={setCurrentValue}
+      label="Gender"
+    />
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
 // import { useRouter } from 'next/router';
-import GenderSelect from '@/components/input/simple-select-input';
+import { useState } from 'react';
+
+import { GenderSelect } from '@/components/input/simple-select-input';
 
 const Index = () => {
   // const router = useRouter();
+  const [gender, setGender] = useState('');
 
   return (
     <div>
-      <GenderSelect />
+      <GenderSelect currentValue={gender} setCurrentValue={setGender} />
     </div>
   );
 };
-
 export default Index;
