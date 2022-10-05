a49e6cc3d9340f54f32ab6e967266371d33d37da
Thanhhau05 <nguyenthanhhau.contact@gmail.com>
2022-10-05 09:22:21 +0700

    Add component button
    (cherry picked from commit 445fd7933bad25e8d7ef7cec6ed212d5c8630560)

---
+++ src/components/constants/select-options.ts
@@ -0,0 +1,22 @@
+export interface SelectOptionObject {
+  label: string;
+  value: string;
+}
+
+export const GENDER_OPTIONS: SelectOptionObject[] = [
+  {
+    label: 'Male',
+    value: 'male',
+  },
+  {
+    label: 'Female',
+    value: 'female',
+  },
+];
+
+export const BUTTON_OPTIONS: SelectOptionObject[] = [
+  {
+    label: 'Calulate',
+    value: 'calulate',
+  },
+];
