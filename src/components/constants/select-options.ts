3390b0a6606cebc22b5d29c74d321354b537ed18
ThanhhauC <nguyenthanhhau.contact@gmail.com>
2022-10-05 08:32:35 +0700

    Add component simple Input

--- src/components/constants/select-options.ts
+++ src/components/constants/select-options.ts
@@ -3,6 +3,14 @@ export interface SelectOptionObject {
   value: string;
 }
 
+export interface SelectOptionInput {
+  type: string;
+  unit: string;
+  maxvalue: number;
+  notification: string;
+  placeholder: string;
+}
+
 export const GENDER_OPTIONS: SelectOptionObject[] = [
   {
     label: 'Male',
@@ -18,5 +26,35 @@ export const BUTTON_OPTIONS: SelectOptionObject[] = [
   {
     label: 'Calulate',
     value: 'calulate',
+  },
+];
+
+export const INPUT_OPTIONS_HEIGHT: SelectOptionInput[] = [
+  {
+    type: 'number',
+    unit: 'cm',
+    maxvalue: 3,
+    notification: 'Please enter the correct height',
+    placeholder: 'Ex: 180',
+  },
+];
+
+export const INPUT_OPTIONS_AGE: SelectOptionInput[] = [
+  {
+    type: 'number',
+    unit: '',
+    maxvalue: 3,
+    notification: 'Please enter the correct age',
+    placeholder: 'Ex: 20',
+  },
+];
+
+export const INPUT_OPTIONS_TEXT: SelectOptionInput[] = [
+  {
+    type: 'text',
+    unit: '',
+    maxvalue: 180,
+    notification: '',
+    placeholder: '',
   },
 ];
