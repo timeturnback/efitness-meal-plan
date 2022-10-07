0882ccb6775afa6034b73665aebdce89d852b8a0
Thanhhau05 <nguyenthanhhau.contact@gmail.com>
2022-10-07 09:09:18 +0700

    update simple select input can be vertical, horizontal

--- src/components/input/simple-select-input.tsx
+++ src/components/input/simple-select-input.tsx
@@ -8,22 +8,29 @@ export function SimpleSelectInput({
   currentValue,
   setCurrentValue,
   options,
+  column,
 }: {
   label: string;
   currentValue: string;
   setCurrentValue: (value: string) => void;
   options: SelectOptionObject[];
+  column: boolean;
 }) {
   return (
-    <div className="p-2">
-      <span className="mb-2 font-medium text-gray-800">{label}</span>
-      <div className="flex items-center gap-3">
+    <div className="inline-block p-2">
+      <span className="font-medium text-gray-800">{label}</span>
+      <div
+        className={classNames(
+          column ? 'block items-center' : 'flex items-center gap-3 my-3'
+        )}
+      >
         {options.map((item) => (
           <SelectOption
             key={item.value}
             data={item}
             currentValue={currentValue}
             onClick={setCurrentValue}
+            column={column}
           />
         ))}
       </div>
@@ -35,10 +42,12 @@ const SelectOption = ({
   onClick,
   data,
   currentValue,
+  column,
 }: {
   onClick: (value: string) => void;
   data: SelectOptionObject;
   currentValue: string;
+  column: boolean;
 }) => {
   const { value, label } = data || {};
   const isSelected = value === currentValue;
@@ -49,7 +58,10 @@ const SelectOption = ({
 
   return (
     <button
-      className="flex items-center rounded-md bg-white p-3 drop-shadow-md"
+      className={classNames(
+        'flex items-center p-3 bg-white rounded-md drop-shadow-md',
+        column && 'my-3'
+      )}
       onClick={_onClick}
     >
       <div className="relative flex items-center border-black pl-7 leading-4 before:absolute before:left-0 before:h-5 before:w-5 before:rounded-3xl before:border before:border-bl-ccc before:content-['']">
@@ -68,9 +80,11 @@ const SelectOption = ({
 export const GenderSelect = ({
   currentValue,
   setCurrentValue,
+  column,
 }: {
   currentValue: string;
   setCurrentValue: (value: string) => void;
+  column: boolean;
 }) => {
   return (
     <SimpleSelectInput
@@ -78,6 +92,7 @@ export const GenderSelect = ({
       currentValue={currentValue}
       setCurrentValue={setCurrentValue}
       label="Gender"
+      column={column}
     />
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
 // import { useRouter } from 'next/router';
 import { useState } from 'react';
 
-import { GenderSelect } from '@/components/input/simple-select-input';
+import { GENDER_OPTIONS } from '@/components/constants/select-options';
+import { SimpleSelectInput } from '@/components/input/simple-select-input';
 
 const Index = () => {
   // const router = useRouter();
   const [gender, setGender] = useState('');
 
   return (
-    <div>
-      <GenderSelect currentValue={gender} setCurrentValue={setGender} />
-    </div>
+    <SimpleSelectInput
+      label="Gender"
+      setCurrentValue={setGender}
+      currentValue={gender}
+      options={GENDER_OPTIONS}
+      column={true}
+    />
   );
 };
 export default Index;
