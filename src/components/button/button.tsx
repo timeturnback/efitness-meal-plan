0a428600eee9d1d36cd78047fdd8b0dcad64c790
Thanhhau05 <nguyenthanhhau.contact@gmail.com>
2022-10-09 09:54:51 +0700

    update button loading

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,34 +1,36 @@
-import 'react-loading-skeleton/dist/skeleton.css';
-
 import { useEffect, useState } from 'react';
-import Skeleton from 'react-loading-skeleton';
 
 export function Button({
+  label,
   value,
   onClick,
 }: {
-  value: string;
+  label: string;
+  value: string[];
   onClick: () => void;
 }) {
   const [loading, setLoading] = useState(false);
   useEffect(() => {
-    const timer = setTimeout(async () => {
+    if (value.length !== 0 && value.find((e) => e !== '') !== undefined) {
       setLoading(true);
-    }, 2000);
-    return () => clearTimeout(timer);
-  });
+      if (value.every((e) => e !== '')) setLoading(false);
+    }
+  }, [value]);
+
   return (
     <div className="p-2">
-      {loading ? (
+      {!loading ? (
         <button
           onClick={onClick}
-          className="duration-400 block h-12 w-48 rounded-md bg-white px-16 py-3 tracking-wide shadow-inner drop-shadow-md transition-colors hover:bg-teal-50/40 hover:shadow-inner"
+          className="duration-400 block h-12 w-48 rounded-md bg-white py-3 tracking-wide shadow-inner drop-shadow-md transition-colors hover:bg-teal-50/40 hover:shadow-inner"
         >
-          {value}
+          {label}
         </button>
       ) : (
-        <button className="h-12 w-48">
-          <Skeleton width="100%" height="100%" className="-top-1 rounded-md" />
+        <button className="duration-400 relative flex h-12 w-48 cursor-default justify-center rounded-md bg-white px-1 py-3 tracking-wide shadow-inner drop-shadow-md transition-colors">
+          <span className="mr-1 inline-block h-full w-6 animate-spin rounded-full border-[3px] border-gray-700 border-l-white" />
+          {label}
+          <span className="absolute top-0 h-full w-full rounded-md bg-white opacity-40" />
         </button>
       )}
     </div>
