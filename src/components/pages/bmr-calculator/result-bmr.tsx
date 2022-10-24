7af3cc738ebe9074799e5d2a8d92ef984c4d39e4
Thanhhau05 <nguyenthanhhau.contact@gmail.com>
2022-10-24 09:29:07 +0700

    update page bmr calculator

---
+++ src/components/pages/bmr-calculator/result-bmr.tsx
@@ -0,0 +1,31 @@
+export const ResultBMR = ({ bmr, tdee }: { bmr: number; tdee: number }) => {
+  return (
+    <div className="w-3/4 py-4 text-gray-800">
+      <div className="px-7">
+        <h2 className="text-2xl font-medium">Your Result</h2>
+        <span>
+          All are in <strong className="font-medium">calories/day</strong>
+        </span>
+        <ContentSpan label="BMR">{bmr}</ContentSpan>
+        <ContentSpan label="TDEE">{tdee}</ContentSpan>
+      </div>
+    </div>
+  );
+};
+
+const ContentSpan = ({
+  children,
+  label,
+}: {
+  children: number;
+  label: string;
+}) => {
+  return (
+    <div className="flex items-center justify-between py-7">
+      <h3 className="text-2xl font-medium">You {label} is:</h3>
+      <span className="text-4xl font-bold uppercase text-gray-900 drop-shadow-md">
+        <span className="text-5xl">{children}</span> cal
+      </span>
+    </div>
+  );
+};
