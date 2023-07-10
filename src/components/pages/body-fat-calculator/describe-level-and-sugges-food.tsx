e4735fa11be9eaadd8ab5237d5392409486e877d
ThanhHau05 <nguyenthanhhau.contact@gmail.com>
2023-07-10 17:05:24 +0700

    update frame for page body fat calcualtor

--- src/components/pages/body-fat-calculator/describe-level-and-sugges-food.tsx
+++ src/components/pages/body-fat-calculator/describe-level-and-sugges-food.tsx
@@ -1,7 +1,12 @@
 export const DescribeLevelAndSuggestFoods = () => {
   return (
-    <div className="h-screen max-w-5xl mx-auto border-2 border-gray-800/90 bg-zinc-100/40 rounded-xl mb-20">
-      <h2>HIIIIIIIIIIIIIIIIIII</h2>
+    <div className="h-full">
+      <h2 className="text-lg">
+        Your body fat percentage is{' '}
+        <span className="font-medium">{'{value}'}%</span> and you fall into the{' '}
+        <span className="font-medium">{'{level} ex:Average'}</span> category.
+        Here is a description of this category and the essential foods for you.
+      </h2>
     </div>
   );
 };
